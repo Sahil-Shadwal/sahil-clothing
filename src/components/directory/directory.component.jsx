0356bf835be2dee026df5e016335db4d36f824f1
Sahil-Shadwal <sahilshadwal@gmail.com>
2023-08-25 11:59:27 +0530

    broken our homepage into smaller components, finished styling our menu-item components

---
+++ src/components/directory/directory.component.jsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import MenuItem from '../menu-item/menu-item.components';
+import './directory.styles.scss';
+
+class Directory extends React.Component {
+    constructor() {
+        super();
+
+        this.state = {
+            sections: 
+            [    {
+                  title: 'hats',
+                  imageUrl: 'https://images.unsplash.com/photo-1534215754734-18e55d13e346?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fGhhdHxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60',
+                  id: 1,
+                  linkUrl: 'shop/hats'
+                },
+                {
+                  title: 'jackets',
+                  imageUrl: 'https://images.unsplash.com/photo-1601924994987-69e26d50dc26?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80',
+                  id: 2,
+                  linkUrl: 'shop/jackets'
+                },
+                {
+                  title: 'sneakers',
+                  imageUrl: 'https://images.unsplash.com/photo-1552066344-2464c1135c32?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80',
+                  id: 3,
+                  linkUrl: 'shop/sneakers'
+                },
+                {
+                  title: 'womens',
+                  imageUrl: 'https://images.unsplash.com/photo-1675215452934-ab9954f0aa78?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8NzF8SU5iRTdSN3FJd2N8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
+                  size: 'large',
+                  id: 4,
+                  linkUrl: 'shop/womens'
+                },
+                {
+                  title: 'mens',
+                  imageUrl: 'https://images.unsplash.com/photo-1580643695733-e502a76bef51?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1932&q=80',
+                  size: 'large',
+                  id: 5,
+                  linkUrl: 'shop/mens'
+                }
+            ]
+        }
+    }
+
+    render() {
+        return (
+            <div className='directory-menu'>
+            {this.state.sections.map(({ title, imageUrl, id, size }) => (
+                <MenuItem key = {id} title={title} imageUrl={imageUrl} size={size}/>
+            ))}
+                </div>
+        )
+    }
+}
+export default Directory;
